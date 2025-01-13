909e06b52e04443a48b50412eba316f4c3d204b0
Joshua Chasnov <joshuachasnov@Joshuas-MacBook-Pro.local>
2025-01-12 17:02:50 -0800

    boilerplate

---
+++ include/wulkan.hpp
@@ -0,0 +1,19 @@
+#ifndef wulkan_WULKAN_HPP
+#define wulkan_WULKAN_HPP
+
+#include "wulkan_internal.hpp"
+#include "instance.hpp"
+#include "debug_messenger.hpp"
+#include "surface.hpp"
+#include "device.hpp"
+#include "swapchain.hpp"
+#include "render_pass.hpp"
+#include "framebuffer.hpp"
+#include "command_pool.hpp"
+#include "command_buffer.hpp"
+#include "semaphore.hpp"
+#include "fence.hpp"
+#include "shader.hpp"
+#include "pipeline.hpp"
+
+#endif
